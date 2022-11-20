06b19f16776be43298099be0e872a437f1ceb8f9
seungchull <seungtech96@gmail.com>
2022-11-21 02:07:53 +0900

    exit 명령이 나올 때까지 계속입력

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -8,10 +8,17 @@ public class Main {
 
         System.out.println("==게시판 v 0.1 ==");
         System.out.println("==프로그램 시작 ==");
-        System.out.printf("명령 : ");
-        String cmd = sc.nextLine();
 
-        System.out.printf("입력된 명령어 : %s\n", cmd);
-        System.out.println("==프로그램 종료 ==");
+        while(true){
+            System.out.printf("명령 : ");
+            String cmd = sc.nextLine();
+
+            System.out.printf("입력된 명령어 : %s\n", cmd);
+            if(cmd.equals("exit")){ //cmd == exit --> 이렇게 작성 x
+                System.out.println("== 프로그램 종료 ==");
+                break;
+            }
+        }
+        sc.close(); // 스캐너 종료
     }
 }
